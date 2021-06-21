c29bde98bc78c9611d0f5df600d348d02b15acf4
jo-narare <nare6004@gmail.com>
2021-06-22 00:01:21 +0900

    소셜 아이디 필드명 수정

--- src/main/java/com/example/security/domain/Member.java
+++ src/main/java/com/example/security/domain/Member.java
@@ -11,15 +11,15 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString(of = {"id", "oAuth2Id", "email", "password", "nickname"})
+@ToString(of = {"id", "socialId", "email", "password", "nickname"})
 public class Member {
 
     @Id @GeneratedValue
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "oauth2_id", unique = true)
-    private String oAuth2Id;
+    @Column(name = "social_id", unique = true)
+    private String socialId;
 
     private String password;
 
