67e9b2e2643c6e0f84ad0d13649f56b8a2ba05c1
jo-narare <nare6004@gmail.com>
2021-06-22 19:46:00 +0900

    불필요 소스 삭제

--- src/main/java/com/example/security/domain/MemberDetail.java
+++ src/main/java/com/example/security/domain/MemberDetail.java
@@ -9,8 +9,4 @@ public class MemberDetail extends User {
     public MemberDetail(String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
     }
-
-    public MemberDetail(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
-    }
 }
